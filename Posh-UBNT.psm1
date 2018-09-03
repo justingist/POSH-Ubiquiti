7c52aee3039adc316943028787a75ab5f72762c9
justingist <justin@justingist.com>
2018-09-03 11:12:34 +0100

    full credits
    added more credit!

--- Posh-UBNT.psm1
+++ Posh-UBNT.psm1
@@ -1,4 +1,5 @@
 # Many thanks to this git repo for the key and basic logic! https://github.com/zhangyoufu/unifi-backup-decrypt
+# Also AES credit: https://gist.github.com/ctigeek/2a56648b923d198a6e60
 #Usage: Decrypt-UBNTBKP -ubntbkp .\pathtomy.unf
 #Output will be a tar.gz in same directory as your initial file. Use your favorite archive tool to open, such as 7-zip.
 
@@ -12,7 +13,7 @@ Param($ubntbkp)
     $iv = [system.text.encoding]::UTF8.GetBytes("ubntenterpriseap")
     $aesManaged = Create-AesManagedObject -key $key -IV $iv
     $unencryptedData = $decryptor.TransformFinalBlock($encryptedstring, 16, $encryptedstring.Length - 16)
-        $aesManaged.Dispose()
+    $aesManaged.Dispose()
 
     [io.file]::WriteAllBytes($outfile,$unencryptedData)
 
