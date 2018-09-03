176489d1badb00e03bfb77eb8aead4c1a5f3d16b
justingist <justin@justingist.com>
2018-09-03 11:08:54 +0100

    created quick decryptor
    This is a quick decryptor for .unf files from Ubiquiti Unifi

---
+++ Posh-UBNT.psm1
@@ -0,0 +1,19 @@
+# Many thanks to this git repo for the key and basic logic! https://github.com/zhangyoufu/unifi-backup-decrypt
+#Usage: Decrypt-UBNTBKP -ubntbkp .\pathtomy.unf
+#Output will be a tar.gz in same directory as your initial file. Use your favorite archive tool to open, such as 7-zip.
+
+Function Decrypt-UBNTBKP {
+Param($ubntbkp)
+    $fullpath = (get-item $ubntbkp).FullName
+    $encryptedstring = get-content $fullpath -Encoding Byte
+    $outfile = $fullpath.Replace('.unf','.tar.gz')
+    $key = [system.text.encoding]::UTF8.GetBytes("bcyangkmluohmars")
+
+    $iv = [system.text.encoding]::UTF8.GetBytes("ubntenterpriseap")
+    $aesManaged = Create-AesManagedObject -key $key -IV $iv
+    $unencryptedData = $decryptor.TransformFinalBlock($encryptedstring, 16, $encryptedstring.Length - 16)
+        $aesManaged.Dispose()
+
+    [io.file]::WriteAllBytes($outfile,$unencryptedData)
+
+}
